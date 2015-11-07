3d515266a8704e85b9b294d2606c3e6eaca5419d
bowerth <bo.werth@gmail.com>
2015-11-07 18:06:19 +0100

    add metadata extraction to GENESIS

--- R/genesisAPI.R
+++ R/genesisAPI.R
@@ -49,6 +49,21 @@
 #'
 #' ## convert to xts object, e.g. for use in \code{dygraphs} package
 #' data.xts <- genesisDFtoXTS(data = data.df)
+#'
+#' ## extract metadata
+#' api.param.datenexport.meta <- 
+#'  api.param.datenexport
+#'
+#' api.param.datenexport.meta[["werte"]] <- "false"
+#' api.param.datenexport.meta[["zusatz"]] <- "true"
+#' 
+#' xml.list.datenexport.meta <- genesisAPI(api.param = api.param.datenexport.meta,
+#'                                         service = "ExportService",
+#'                                         curl = ui.apiGENESIS.curl
+#'                                         )
+#' 
+#' meta.df <- genesisXMLtoDF(xml.list = xml.list.datenexport.meta,
+#'                           meta = TRUE)
 
 genesisAPI <- function(
     api.param = stop("'api.param' must be specified"),
@@ -85,20 +100,54 @@ genesisAPI <- function(
 
 
 #' @rdname genesisAPI
+#' @param s metadata string to convert to data frame
+#' @export
+metaClean <- function(s) {
+    s <- s[substring(s, 1, 2) == "D;"]
+    X <- strsplit(s, split = ";")
+    res <- data.frame(code = sapply(X, "[[", 2),
+                      label = sapply(X, "[[", 3))
+    return(res)
+  }
+
+#' @rdname genesisAPI
 #' @param xml.list a character string returned from
 #' @export
 genesisXMLtoDF <- function(
-    xml.list = stop("'xml.list' must be provided")
+  xml.list = stop("'xml.list' must be provided"),
+  meta = FALSE
 ) {
 
-    ## xml.list <- xml.list.datenexport
+  ## xml.list <- xml.list.datenexport
 
-    if (names(xml.list[[1]]) == "DatenExportResponse") {
-        data.str <- xml.list[["Body"]][["DatenExportResponse"]][["DatenExportReturn"]][["quader"]][["quader"]][["quaderDaten"]]
-    } else {
-        stop("format not specified\n")
+  if (names(xml.list[[1]]) == "DatenExportResponse") {
+    data.str <- xml.list[["Body"]][["DatenExportResponse"]][["DatenExportReturn"]][["quader"]][["quader"]][["quaderDaten"]]
+  } else {
+    stop("format not specified\n")
+  }
+
+  if (meta==TRUE) {
+    data.char <- strsplit(data.str, "\n")[[1]]
+    ## namedim.begin <- match("K;ERH-D;GUELT-AB;GUELT-BIS;PZT", data.char)
+      ## namemeasure.begin <- match("K;ME;ME-NAME;KTX;TYP;GRUND-ME;UMR-ME;POTENZ;FAKTOR;TRANS-FLAG-2;ME-NAME-2;KTX-2;NOTIZEN;DEF;DEF-2", data.char)
+      dimensionlabel.begin <- match("K;MM;NAME;KTX;MM-TYP;BESTAND;SPR-TMP;OBER-BGR-JN;GUELT-AB;GUELT-BIS;GLIED-TYP;STD-SORT;VBD-SCHL-NR;GENESIS-VBD;REGIOSTAT;EU-VBD;DST;SUMMIERBAR;TRANS-FLAG-2;KTX-2;LTX;NOTIZEN;DEF;LTX-2;DEF-2", data.char)
+      dimensionmember.begin <- match("K;KMA;FACH-SCHL;KTX;KLASS-SCHL;VBD-SCHL-NR;GENESIS-VBD;REGIOSTAT;EU-VBD;TRANS-FLAG-2;KTX-2;LTX;DEF;NOTIZEN;LTX-2;DEF-2", data.char)
+      dimensionmember.end <- match("K;KMAZ;FACH-SCHL;NAME;GUELT-AB;GUELT-BIS;POS-NR;SPR-TMP", data.char)
+
+      dimensionlabel.raw <- data.char[c((dimensionlabel.begin + 1):(dimensionmember.begin - 1))]
+
+      dimensionmember.raw <- data.char[c((dimensionmember.begin + 1):(dimensionmember.end - 1))]
+
+    dimensionlabel <- metaClean(s = dimensionlabel.raw)
+    dimensionmember <- metaClean(dimensionmember.raw)
+    
+    result_list <- list(
+      label = dimensionlabel,
+      member = dimensionmember)
+    
+      return(result_list)
     }
-
+    
     ## str(xml.list)
     ## substr(data.str, 1, 100)
     ## substr(data.str, 1, 500)
